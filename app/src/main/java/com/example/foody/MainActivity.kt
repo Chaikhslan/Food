72b6d6b31548067d526f4a67e039a16a40bdf588
Nauryz <kasymbek.n@chocomart.kz>
2020-12-25 18:44:08 +0600

    first commit

---
+++ app/src/main/java/com/example/foody/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.foody
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.resipesFragment_menu,
+                R.id.favoriteRecipesFragment_menu,
+                R.id.foodJokeFragment_menu
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || return super.onSupportNavigateUp()
+    }
+}
